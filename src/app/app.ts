1f912b018f248e399dc4fa78c3197665653781b0
EdwinNino-24 <edwinnino2404@gmail.com>
2025-09-16 11:11:53 -0500

    v1.0

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { Header } from './components/header/header';
+import { Player } from './components/player/player';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    Header,
+    Player,
+    Footer
+],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
