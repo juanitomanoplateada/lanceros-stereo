5878f6569ad5d975f4c9e135c65812c091c611f6
EdwinNino-24 <edwinnino2404@gmail.com>
2025-09-17 16:53:23 -0500

    v2.7

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
   ]
 };
