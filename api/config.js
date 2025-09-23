a573e6175a9e6d689dfd7461f34de8e287155319
EdwinNino-24 <edwinnino2404@gmail.com>
2025-09-23 11:17:38 -0500

    v3.1

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const config = {
-    streamUrl: process.env.STREAM_URL || "http://link.zeno.fm:80/jz1bfxan45kuv",
+    streamUrl: process.env.STREAM_URL || "https://stream-178.zeno.fm/jz1bfxan45kuv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJqejFiZnhhbjQ1a3V2IiwiaG9zdCI6InN0cmVhbS0xNzguemVuby5mbSIsInRtIjpmYWxzZSwicnR0bCI6NSwianRpIjoiaXExcG1zdEZUbEtvQXo0R2ZPaG5MZyIsImlhdCI6MTc1ODY0NDIxMiwiZXhwIjoxNzU4NjQ0MjcyfQ.edIDbQdW5ZgPGtOe2REF1Y9ZoUa8bej2TLClbESQYG4",
     metadataUrl:
       process.env.METADATA_URL ||
       "https://api.zeno.fm/mounts/metadata/subscribe/jz1bfxan45kuv",
--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -24,7 +24,7 @@ export class ConfigService {
           err
         );
         this.config = {
-          streamUrl: 'http://link.zeno.fm:80/jz1bfxan45kuv',
+          streamUrl: 'https://stream-178.zeno.fm/jz1bfxan45kuv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJqejFiZnhhbjQ1a3V2IiwiaG9zdCI6InN0cmVhbS0xNzguemVuby5mbSIsInRtIjpmYWxzZSwicnR0bCI6NSwianRpIjoiaXExcG1zdEZUbEtvQXo0R2ZPaG5MZyIsImlhdCI6MTc1ODY0NDIxMiwiZXhwIjoxNzU4NjQ0MjcyfQ.edIDbQdW5ZgPGtOe2REF1Y9ZoUa8bej2TLClbESQYG4',
           metadataUrl:
             'https://api.zeno.fm/mounts/metadata/subscribe/jz1bfxan45kuv',
         };
